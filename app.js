36785dc15bb2b980d0c0e4af4302bc440c866568
Ed Gilmore <edgilmore@live.com>
2017-10-09 17:56:03 -0700

    added middleware functions

--- app.js
+++ app.js
@@ -2,15 +2,34 @@
 
 const express = require('express');
 const app = express();
+const jsonParser = require('body-parser').json;
 
 const port = process.env.PORT || 3000;
 
+const jsonCheck = (req, res, next) => {
+    if(req.body){
+        console.log(`The sky is ${req.body.color}`);
+        return res;
+    }
+    else {
+        console.log(`there is no body property on the request`);
+        return res;
+    }
+};
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
+
 app.use((req, res, next) => {
-    console.log('First piece of middleware');
+    req.myMessage = `Hello, middlware #2`;
     next();
 });
 app.use((req, res, next) => {
-    console.log('Second piece of middleware');
+    console.log(`The leaves on the trees are ${req.query.color}`);
+    next();
+});
+app.use((req, res, next) => {
+    console.log(req.myMessage);
     next();
 });
 
