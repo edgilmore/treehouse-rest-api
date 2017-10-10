c92cc15119286502897ec2d0dff4eaede21aa007
Ed Gilmore <edgilmore@live.com>
2017-10-09 17:30:13 -0700

    added middleware

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    console.log('First piece of middleware');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('Second piece of middleware');
+    next();
+});
+
 app.listen(port, () => {
     console.log(`Express server is listening on port: ${port}`);
 });
