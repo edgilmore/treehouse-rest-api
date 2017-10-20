15dad1b14ea883c262b3ee1c2ddebe382355463a
Ed Gilmore <edgilmore@live.com>
2017-10-20 14:36:39 -0700

    added a error handler for 404

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use('/questions', routes);
-
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
 app.use((req, res, next) => {
     req.myMessage = `Hello, middlware #2`;
     next();
