8f07e35514b9bad87e634bfc3cb040fb23f8e458
Ed Gilmore <edgilmore@live.com>
2017-10-20 14:28:25 -0700

    additional routes added to the router

--- routes.js
+++ routes.js
@@ -10,18 +10,50 @@ router.get('/', function (req, res) {
         response: "You sent me a GET request"
     });
 });
-
-router.post('/', function (req, res) {
+//post /questions/:id/answers
+router.post('/:qId/answers', function (req, res) {
     res.json({
-        response: "You sent me a POST request",
+        response: "You sent me a POST request /answers",
+        questionId: req.params.qId,
+        body: req.body
+    });
+});
+
+//put /questions/:id/answers/:id
+//edit a specific answer
+router.put('/:qId/answers/:aId', function (req, res) {
+    res.json({
+        response: 'You sent me a PUT request to /answers',
+        questionId: req.params.qId,
+        answerId: req.params.aId,
         body: req.body
     })
 });
-
-router.get('/:id', function(req, res){
+//delete /questions/:qId/answers/:aId
+//delete a specific answer
+router.delete('/:qId/answers/:aId', function (req, res) {
     res.json({
-        response: `You sent me a GET request for an ID ${req.params}`,
-    })
+        response: 'You sent me a DELETE request to /answers',
+        questionId: req.params.qId,
+        answerId: req.params.aId
+    });
+});
+
+//post /questions/:qId/answers/:aId/vote-up
+//post /questions/:qId/answers/:aId/vote-down
+//vote on an answer
+router.post('/:qId/answers/:aId/vote-:dir', function (req, res) {
+    res.json({
+        response: `You sent me a POST request to /vote-${req.params.dir}`,
+        questionId: req.params.qId,
+        answerId: req.params.aId,
+    });
+});
+// get /questions/:qId
+router.get('/:qId', function (req, res) {
+    res.json({
+        response: `You sent me a GET request for an ID ${req.params.qId}`,
+    });
 });
 
 module.exports = router;
