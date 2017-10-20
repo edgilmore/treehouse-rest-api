241b597fe5232dd426d971588bfa8d1a3a058e0b
Ed Gilmore <edgilmore@live.com>
2017-10-20 13:52:54 -0700

    created additional routes in the router

--- routes.js
+++ routes.js
@@ -3,8 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(){
-    
+//get /questions
+router.get('/', function (req, res) {
+    //return all the questions
+    res.json({
+        response: "You sent me a GET request"
+    });
+});
+
+router.post('/', function (req, res) {
+    res.json({
+        response: "You sent me a POST request",
+        body: req.body
+    })
+});
+
+router.get('/:id', function(req, res){
+    res.json({
+        response: `You sent me a GET request for an ID ${req.params}`,
+    })
 });
 
 module.exports = router;
