99cf821c0134148c45ab8274717271cf77cc7dd2
Ed Gilmore <edgilmore@live.com>
2017-12-05 17:31:28 -0700

    added default scheme scenario add additional animals used find query to output details about animals

--- mongo_sandbox.js
+++ mongo_sandbox.js
@@ -6,23 +6,39 @@ mongoose.connect('mongodb://localhost:27017/sandbox');
 
 const db = mongoose.connection;
 
-db.on('error', function(err) {
+db.on('error', function (err) {
     console.error('connection error:', err);
 });
 
-db.once('open', function(){
+db.once('open', function () {
     console.debug('connection successful');
     // all database communication goes here
     const Schema = mongoose.Schema;
     const AnimalScheme = new Schema({
-        type: { type: String, default: 'goldfish'},
-        color: { type: String, default: 'gold'},
-        size: { type: String, default: 'small'},
-        mass: { type: Number, default: '0.007'},
-        name: { type: String,  default: 'goldie'}
+        type: {
+            type: String,
+            default: 'goldfish'
+        },
+        color: {
+            type: String,
+            default: 'gold'
+        },
+        size: {
+            type: String,
+            default: 'small'
+        },
+        mass: {
+            type: Number,
+            default: '0.007'
+        },
+        name: {
+            type: String,
+            default: 'goldie'
+        }
     });
     // create model for the animal scheme in mongo
     const Animal = mongoose.model('Animal', AnimalScheme);
+    const animal = new Animal({});
     // create animal document
     const elephant = new Animal({
         type: 'elephant',
@@ -31,14 +47,33 @@ db.once('open', function(){
         mass: 6000,
         name: 'Larry',
     });
+    const whale = new Animal({
+        type: 'whale',
+        size: 'big',
+        mass: 190500,
+        name: 'Fig',
+    })
     // save the object
-    elephant.save(function(err){
-        if(err) console.error('save failed');
-        else console.debug('Saved!');
-        // close db
-        db.close(function(){
-            console.debug('connection closed');
+    elephant.save(function (err) {
+        if (err) console.error(err);
+        animal.save(function(err) {
+            if (err) console.error(err);
+            whale.save(function(err){
+                if(err) console.error(err);
+                Animal.find({size: 'big'}, function(err, animals){
+                    animals.forEach(function(animal){
+                        console.log(animal.name + ' the ' + animal.color + ' ' + animal.type);
+                    });
+                    closeDataStore(db);
+                });
+            })
         });
     });
-    
+
 });
+// close db
+function closeDataStore(db) {
+    db.close(function () {
+        console.debug('connection closed');
+    });
+}
